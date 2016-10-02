cc2f7bfa88c626ce7f107d1ae68f6bd3756b0556
nikohansen <nikolaus.hansen@gmail.com>
2016-10-02 21:43:09 +0200

    added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,54 @@
+# from distutils.core import setup
+from setuptools import setup
+from cma import __version__  # assumes that the right module is visible first in path, i.e., cma folder is in current folder
+
+# prevent the error when building Windows .exe
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func)
+
+# packages = ['cma'],  # indicates a multi-file module and that we have a cma folder and cma/__init__.py file
+
+try:
+    with open('README.txt') as file:
+        long_description = file.read()  # now assign long_description=long_description below
+except IOError:  # file not found
+    long_description = ""
+
+setup(name="cma",
+      long_description=long_description,  # __doc__, # can be used in the cma.py file
+      version=__version__.split()[0],
+      description="CMA-ES, Covariance Matrix Adaptation " +
+                  "Evolution Strategy for non-linear numerical " +
+                  "optimization in Python",
+      author="Nikolaus Hansen",
+      author_email="authors firstname.lastname at inria dot fr",
+      maintainer="Nikolaus Hansen",
+      maintainer_email="authors firstname.lastname at inria dot fr",
+      url="https://www.lri.fr/~hansen/cmaes_inmatlab.html#python",
+      # license="MIT",
+      license="BSD",
+      classifiers = [
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Education",
+          "Intended Audience :: Other Audience",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Artificial Intelligence",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "License :: OSI Approved :: BSD License",
+          # "License :: OSI Approved :: MIT License",
+      ],
+      keywords=["optimization", "CMA-ES", "cmaes"],
+      packages=["cma"],
+      requires=["numpy"],
+)
