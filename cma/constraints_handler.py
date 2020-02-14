0022cd8a487808f637817dd0d8c69fcf336dba14
nikohansen <nikolaus.hansen@inria.fr>
2020-02-14 14:07:08 +0100

    augmented Lagrangian constraints handling

--- cma/constraints_handler.py
+++ cma/constraints_handler.py
@@ -3,9 +3,12 @@
 """
 from __future__ import absolute_import, division, print_function  #, unicode_literals
 # __package__ = 'cma'
+import warnings as _warnings
 import numpy as np
+from numpy import logical_and as _and, logical_or as _or, logical_not as _not
 from .utilities.utils import rglen
-# from .utilities.math import Mh
+from .utilities.math import Mh as _Mh
+from . import logger as _logger
 from .transformations import BoxConstraintsLinQuadTransformation
 from .utilities.python3for2 import range
 del absolute_import, division, print_function  #, unicode_literals
@@ -469,3 +472,306 @@ class BoundPenalty(BoundaryHandlerBase):
         # ## return penalty
         # es.more_to_write = self.gamma if not np.isscalar(self.gamma) else N*[1]
         return self  # bound penalty values
+
+class PopulationEvaluator(object):
+    """evaluate and store f- and g-values of a population"""
+    def __init__(self, objective, constraints):
+        self.objective = objective
+        self.constraints = constraints
+    def __call__(self, X, **kwargs):
+        """`kwargs` are named solutions resulting in::
+
+            self.name['x'] = kwargs[name]
+            self.name['f'] = self.objective(kwargs[name])
+            self.name['g'] = self.constraints(kwargs[name])
+
+        """
+        self.X = X
+        self.F = [self.objective(x) for x in X]
+        self.G = [self.constraints(x) for x in X]
+        for name, x in kwargs.items():
+            setattr(self, name, {'x': x,
+                                 'f': self.objective(x),
+                                 'g': self.constraints(x)})
+        return self
+    @property
+    def feasibility_ratios(self):
+        """or bias for equality constraints"""
+        return np.mean(np.asarray(self.G) <= 0, axis=0)
+        # return [np.mean(g <= 0) for g in np.asarray(self.G).T]
+
+class AugmentedLagrangian(object):
+    """Augmented Lagrangian with adaptation of the coefficients
+
+    for minimization, implemented after Atamna et al FOGA 2017,
+    Algorithm 1 and Sect 8.2, https://hal.inria.fr/hal-01455379/document.
+
+    Input `dimension` is the search space dimension, boolean `equality`
+    may be an iterable of length of number of constraints indicating the
+    type for each constraint.
+
+    Below, the objective function value is denoted as ``f = f(x)``, the
+    constraints values as ``g = g(x) <= 0``, the penalties compute to
+    ``penalties(x) = self(g(x)) = lam g + mu g^2 / 2`` (if g > -lam / mu) for
+    each element of g, as returned by calling the instance with g as argument.
+
+    The penalized "fitness" value ``f + sum(self(g))`` shall be minimized.
+    lam and mu are the Lagrange multipliers or coefficients.
+
+    An additional method, `set_coefficients` allows to initialize the
+    Lagrange multipliers from data.
+
+    A short example (and doctest):
+
+    >>> import cma
+    >>> from cma.constraints_handler import AugmentedLagrangian, PopulationEvaluator
+    >>> m = 2  # number of constraints
+    >>> def objective(x):
+    ...     return sum(x[m:]**2) + sum((x[:m] - 1)**2) - m
+    >>> def constraints(x):
+    ...     return x[:m]
+    >>> es = cma.CMAEvolutionStrategy(3 * [1], 1, {
+    ...          'termination_callback': lambda es: sum(es.mean**2) < 1e-8})  #doctest: +ELLIPSIS
+    (3_w,7)-aCMA-ES...
+    >>> al = AugmentedLagrangian(es.N)  # lam and mu still need to be set
+    >>> al.chi_domega = 1.1  # seems to give better results than the default setting
+    >>> # al.lam, al.mu = ...  # we could set the initial Lagrange coefficients here
+    >>> while not es.stop():
+    ...     eva = PopulationEvaluator(objective, constraints)(es.ask(), m=es.mean)
+    ...     al.set_coefficients(eva.F, eva.G)  # set lam and mu, not part of the original algorithm
+    ...     al.update(eva.m['f'], eva.m['g'])
+    ...     es.tell(eva.X, [f + sum(al(g)) for f, g in zip(eva.F, eva.G)])
+    >>> if es.result.evaluations > 3100:
+    ...     print("evaluations %d !< 3100. 1500 is normal, 2700 happens rarely" % es.result.evaluations)
+    >>> assert 'callback' in es.stop()
+    >>> assert len(eva.feasibility_ratios) == m
+    >>> assert sum(eva.feasibility_ratios < 0) == sum(eva.feasibility_ratios > 1) == 0
+
+    Details: the input `dimension` is needed to set change rate
+    `chi_domega`, to compute initial coefficients and to compare between h
+    and g to update mu. The dependency of `chi_domega` on the dimension may
+    be however suboptimal. Setting ``self.chi_domega = 1.1`` seems to give
+    better results than the default setting.
+
+    """
+    def __init__(self, dimension, equality=False):
+        """when given use search space `dimension` to set chi_domega to the original (worse) setting"""
+        self.dimension = dimension  # maybe not desperately needed
+        self.lam, self.mu = None, None  # will become np arrays
+        self._initialized = np.array(False)  # only used for setting, not for update
+        self._equality = np.array(equality, dtype=bool)
+        self.k1 = 3
+        self.k2 = 5
+        self.dgamma = 5  # damping for lambda change
+        self.chi_domega = 2**(1. / 5 / dimension)  # factor for mu change, 5 == domega
+        # self.chi_domega = 2**0.15  # seems to work better than the default
+        self.f, self.g = 2 * [None]  # store previous values
+        self.count = 0  # number of actual updates after any mu > 0 was set
+
+        self.lam_opt = None  # only for display in logger
+        self.logging = True
+        self._init_()
+
+    def _init_(self):
+        """allow to reset the logger with a single call"""
+        self.logger = _logger.Logger(self,
+            callables=[lambda s: s.lam if s.lam_opt is None else np.log10(np.abs(s.lam - s.lam_opt) + 1e-9),
+                       lambda s: np.log10(s.mu + 1e-9)],
+            labels=['lambda' if self.lam_opt is None else 'lg(lambda-lam_opt)',
+                    'lg(mu)'],
+            name='outauglag',
+            )
+        self.logger_mu_conditions = _logger.Logger("mu_conditions", labels=[
+                        r'$\mu$ increases',
+                        r'$\mu g^2 < %.0f |\Delta h| / n$' % self.k1,
+                        r'$|\Delta g| < |g| / %.0f$' % self.k2])
+
+    @property
+    def m(self):
+        """number of constraints, raise `TypeError` if not set yet"""
+        return len(self.lam)
+
+    @property
+    def feasibility_ratios(self):
+        """or bias for equality constraints, versatile interface may change"""
+        try: return [np.mean(np.asarray(g) <= 0) for g in np.asarray(self.G).T]
+        except AttributeError: return None
+
+    def set_m(self, m):
+        """initialize attributes which depend on the number of constraints.
+
+        This requires the `lam` attribute to be `None` and deletes all
+        previously set or adapted `mu` coefficients.
+        """
+        assert self.lam is None  # better safe than sorry
+        self.lam = np.zeros(m)
+        self.mu = np.zeros(m)
+        self._initialized = np.zeros(m, dtype=bool)
+
+    def _check_dtypes(self):
+        """in case the user set the attributes"""
+        for name in ['mu', 'lam']:
+            if getattr(self, name).dtype != 'float':
+                setattr(self, name, np.array(getattr(self, name), dtype='float'))
+        assert self._initialized.dtype == 'bool'
+
+    def set_coefficients(self, F, G):
+        """compute initial coefficients based on some f- and g-values.
+
+        The formulas to set the coefficients::
+
+            lam = iqr(f) / (sqrt(n) * iqr(g))
+            mu = 2 * iqr(f) / (5 * n * (iqr(g) + iqr(g**2)))
+
+        are taken out of thin air and not thoroughly tested. They are
+        additionally protected against division by zero.
+
+        Each row of `G` represents the constraints of one sample measure.
+
+        Set lam and mu until a population contains more than 10% infeasible
+        and more than 10% feasible at the same time. Afterwards, this at least...?...
+        """
+        self.F, self.G = F, G  # versatile storage
+        if self.mu is not None and all(self._initialized * (self.mu > 0)):
+            return  # we're all set
+        G = np.asarray(G).T  # now row G[i] contains all values of constraint i
+        sign_average = np.mean(np.sign(G), axis=1)  # caveat: equality vs inequality
+        if self.lam is None:  # destroys all coefficients
+            self.set_m(len(G))
+        elif not self._initialized.shape:
+            self.lam_old, self.mu_old = self.lam[:], self.mu[:]  # in case the user didn't mean to
+            self._initialized = np.array(self.m * [self._initialized])
+        self._check_dtypes()
+        idx = _and(_or(_not(self._initialized), self.mu == 0),
+                   _or(sign_average > -0.8, self.isequality))
+        # print(len(F), len(self.G))
+        if np.any(idx):
+            df = _Mh.iqr(F)
+            dG = np.asarray([_Mh.iqr(g) for g in G])      # only needed for G[idx], but like
+            dG2 = np.asarray([_Mh.iqr(g**2) for g in G])  # this simpler in later indexing
+            if np.any(df == 0) or np.any(dG == 0) or np.any(dG2 == 0):
+                _warnings.warn("iqr(f), iqr(G), iqr(G**2)) == %s, %s, %s" % (str(df), str(dG), str(dG2)))
+            assert np.all(df >= 0) and np.all(dG >= 0) and np.all(dG2 >= 0)
+            # 1 * dG2 leads to much too small values for Himmelblau
+            mu_new = 2. / 5 * df / self.dimension / (dG + 1e-6 * dG2 + 1e-11 * (df + 1))  # TODO: totally out of thin air
+            idx_inequ = _and(idx, _not(self.isequality))
+            if np.any(idx_inequ):
+                self.lam[idx_inequ] = df / (self.dimension * dG[idx_inequ] + 1e-11 * (df + 1))
+
+            # take min or max with existing value depending on the sign average
+            # we don't know whether this is necessary
+            isclose = np.abs(sign_average) <= 0.2
+            idx1 = _and(_and(_or(isclose,
+                                 _and(_not(self.isequality), sign_average <= 0.2)),
+                             _or(self.mu == 0, self.mu > mu_new)),
+                             idx)  # only decrease
+            idx2 = _and(_and(_and(_not(isclose),
+                                  _or(self.isequality, sign_average > 0.2)),
+                                  self.mu < mu_new),
+                                  idx)  # only increase
+            idx3 = _and(idx, _not(_or(idx1, idx2)))  # others
+            assert np.sum(_and(idx1, idx2)) == 0
+            if np.any(idx1):  # decrease
+                iidx1 = self.mu[idx1] > 0
+                if np.any(iidx1):
+                    self.lam[idx1][iidx1] *= mu_new[idx1][iidx1] / self.mu[idx1][iidx1]
+                self.mu[idx1] = mu_new[idx1]
+            if np.any(idx2):  # increase
+                self.mu[idx2] = mu_new[idx2]
+            if np.any(idx3):
+                self.mu[idx3] = mu_new[idx3]
+            if 11 < 3:  # simple version, this may be good enough
+                self.mu[idx] = mu_new[idx]
+            self._initialized[_and(idx, _or(self.count > 2 + self.dimension,  # in case sign average remains 1
+                                            np.abs(sign_average) < 0.8))] = True
+        elif all(self._initialized) and all(self.mu > 0):
+            _warnings.warn(
+                "Coefficients are already fully initialized. This can (only?) happen if\n"
+                "the coefficients are set before the `_initialized` array.")
+
+    @property
+    def isequality(self):
+        """bool array, `True` if `i`-th constraint is an equality constraint"""
+        try:
+            len(self._equality)
+        except TypeError:
+            self._equality = np.asarray(self.m * [True if self._equality else False])
+        return self._equality
+
+    def __call__(self, g):
+        """return `list` of AL penalties for constraints values in `g`.
+
+        Penalties are zero in the optimum and can be negative down to
+        ``-lam**2 / mu / 2``.
+        """
+        if self.lam is None:
+            return [0.] * len(g)
+        assert len(self.lam) == len(self.mu)
+        if len(g) != len(self.lam):
+            raise ValueError("len(g) = %d != %d = # of Lagrange coefficients"
+                             % (len(g), len(self.lam)))
+        assert self._equality.dtype == 'bool'
+        idx = _and(_not(self._equality), self.mu * g < -1 * self.lam)
+        if any(idx):
+            g = np.array(g, copy=True)
+            g[idx] = -self.lam[idx] / self.mu[idx]
+        return [self.lam[i] * g[i] + 0.5 * self.mu[i] * g[i]**2
+                for i in range(len(g))]
+
+    def update(self, f, g):
+        """f is a scalar, g is a vector.
+
+        Update Lagrange multipliers based on Atamna et al 2017. f and g are
+        supposed to have been computed from the distribution mean.
+
+        Details: do nothing if Lagrange coefficients `lam` were not yet set.
+        """
+        if self.lam is None:
+            try: self._count_noupdate += 1
+            except AttributeError: self._count_noupdate = 1
+            if self._count_noupdate % (1 + self._count_noupdate**0.5) < 1:
+                _warnings.warn("no update for %d calls (`lam` and `mu` need "
+                               "to be initialized first)" % self._count_noupdate)
+            return
+        if self.m == 0:
+            return
+        self._check_dtypes()
+        if self.g is not None and np.any(self.mu > 0):  # mu==0 makes a zero update anyway
+            assert len(self.lam) == len(self.mu) == len(g)
+            if not self.count and self.chi_domega < 1.05:
+                _warnings.warn("chi_omega=%f as by default, however values <<1.1 may not work well"
+                                % self.chi_domega)
+            dg = np.asarray(g) - self.g
+            dh = f + sum(self(g)) - self.f - sum(self(self.g))  # using the same coefficients
+            for i in range(len(self.lam)):
+                if self.logging:
+                    condk1 = bool(self.mu[i] * g[i]**2 < self.k1 * np.abs(dh) / self.dimension)
+                    condk2 = bool(self.k2 * np.abs(dg[i]) < np.abs(self.g[i]))
+                    self.logger_mu_conditions.add(i - 0.1 + 0.25 * np.asarray(
+                                [max((condk1, condk2)), 1.25 + condk1, 2.5 + condk2]))
+                if self.mu[i] == 0:
+                    continue  # for mu==0 all updates are zero anyway
+                # address when g[i] remains inactive ("large" negative)
+                if not self.isequality[i] and g[i] * self.mu[i] < -self.lam[i]:
+                    continue  # not in the original algorithm
+                    # prevent that lam gets negative and mu diverges temporarily (as observed)
+                # lambda update
+                self.lam[i] += self.mu[i] * g[i] / self.dgamma
+                if not self.isequality[i] and self.lam[i] < 0:  # clamp to zero
+                    # if we stay in the feasible domain, lam would diverge to -inf (as observed)
+                    self.lam[i] = 0
+                    # and mu would diverge to +inf, because abs(g) >> dg -> 0 (as observed)
+                    continue  # don't update mu (it may diverge)
+                # mu update
+                if self.mu[i] * g[i]**2 < self.k1 * np.abs(dh) / self.dimension or (
+                    self.k2 * np.abs(dg[i]) < np.abs(self.g[i])):  # this condition is always true if constraint i is not active
+                    self.mu[i] *= self.chi_domega**0.25  # 4 / 1 is the stationary odds ratio of increment / decrement
+                else:
+                    self.mu[i] /= self.chi_domega
+            self.count += 1
+        assert np.all((self.lam >= 0) + self.isequality)
+        self.f, self.g = f, g  # self(g) == 0 if mu=lam=0
+        if self.logging:
+            self.logger.push()
+            self.logger_mu_conditions.push()
+
