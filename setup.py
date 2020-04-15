bb7e3d1c79fbf0f99a18b0222ccb4e672341445e
nikohansen <nikolaus.hansen@inria.fr>
2020-04-16 01:46:40 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""setup for cma package distribution.
+
+To prepare the docs from a dirty code folder::
+
+    conda activate py27
+    backup cma --move
+    git checkout -- cma
+    pip install -e .
+    pydoctor --docformat=restructuredtext --make-html cma
+    backup --recover
+
+To prepare a distribution from a dirty code folder::
+
+    backup cma --move    # backup is a homebrew minitool
+    git checkout -- cma
+    python setup.py check
+    python setup.py sdist bdist_wininst bdist_wheel --universal > dist_call_output.txt ; less dist_call_output.txt
+    twdiff cma build/lib/cma/  # just checking
+    backup --recover  # recover above moved folder (and backup current, just in case)
+
+Check distribution and project description:
+
+    tree build  # check that the build folders are clean
+    twine check dist/*
+    python setup.py --long-description | rst2html.py > long-description.html ; open long-description.html
+
+Finally upload the distribution::
+
+    twine upload dist/*versionnumber*  # to not upload outdated stuff
+
+"""
 # from distutils.core import setup
 from setuptools import setup
 from cma import __version__  # assumes that the right module is visible first in path, i.e., cma folder is in current folder
@@ -22,14 +55,15 @@ except IOError:  # file not found
 
 setup(name="cma",
       long_description=long_description,  # __doc__, # can be used in the cma.py file
+      long_description_content_type = 'text/markdown',
       version=__version__.split()[0],
       description="CMA-ES, Covariance Matrix Adaptation " +
                   "Evolution Strategy for non-linear numerical " +
                   "optimization in Python",
       author="Nikolaus Hansen",
-      author_email="authors firstname.lastname at inria dot fr",
+      author_email="authors_firstname.lastname@inria.fr",
       maintainer="Nikolaus Hansen",
-      maintainer_email="authors_firstname.lastname@inria.fr",
+      # maintainer_email="authors_firstname.lastname@inria.fr",
       url="https://github.com/CMA-ES/pycma",
       # license="MIT",
       license="BSD",
@@ -41,11 +75,13 @@ setup(name="cma",
           "Topic :: Scientific/Engineering :: Mathematics",
           "Topic :: Scientific/Engineering :: Artificial Intelligence",
           "Operating System :: OS Independent",
-          "Programming Language :: Python :: 2.6",
+          # "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Development Status :: 4 - Beta",
+          "Development Status :: 5 - Production/Stable",
           "Environment :: Console",
+          "Framework :: IPython",
+          "Framework :: Jupyter",
           "License :: OSI Approved :: BSD License",
           # "License :: OSI Approved :: MIT License",
       ],
