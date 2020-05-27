d731c47252c3031e3ecb53040d00f6eea4ae903a
nikohansen <nikolaus.hansen@inria.fr>
2020-05-27 16:26:47 +0200

    bringing back s.figsave due to #131, replace print with warn

--- cma/s.py
+++ cma/s.py
@@ -6,6 +6,12 @@ Provides various aliases from within the `cma` package, to be reached like
 
 Don't use for stable code.
 """
+import warnings as _warnings
+try: from matplotlib import pyplot as _pyplot  # like this it doesn't show up in the interface
+except ImportError:
+    _pyplot = None
+    _warnings.warn('Could not import matplotlib.pyplot, therefore'
+                   ' ``cma.plot()`` etc. is not available')
 # from . import fitness_functions as ff
 from . import evolution_strategy as es
 from . import fitness_transformations as ft
@@ -17,18 +23,21 @@ from .utilities.utils import pprint
 from .utilities.math import Mh
 # from .fitness_functions import elli as felli
 
-def figshow():
-    """`pyplot.show` to make a plotted figure show up"""
-    # is_interactive = matplotlib.is_interactive()
-    from matplotlib import pyplot  # like this it doesn't show up in the interface
-    pyplot.ion()
-    pyplot.show()
-    # if we call now matplotlib.interactive(True), the console is
-    # blocked
+if _pyplot:
+    def figshow():
+        """`pyplot.show` to make a plotted figure show up"""
+        # is_interactive = matplotlib.is_interactive()
+
+        _pyplot.ion()
+        _pyplot.show()
+        # if we call now matplotlib.interactive(True), the console is
+        # blocked
+    figsave = _pyplot.savefig
+
 if 11 < 3:
     try:
         from matplotlib.pyplot import savefig as figsave, close as figclose, ion as figion
     except:
         figsave, figclose, figshow = 3 * ['not available']
-        print('Could not import matplotlib.pyplot, therefore ``cma.plot()``' +
-              ' etc. is not available')
+        _warnings.warn('Could not import matplotlib.pyplot, therefore ``cma.plot()``'
+                       ' etc. is not available')
